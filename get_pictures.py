531d478e8419328e8864123894849a77650e105f
Jesus Esparza <jesusesparza242@gmail.com>
2024-02-15 20:34:34 -0600

    video record functionality added

--- get_pictures.py
+++ get_pictures.py
@@ -14,50 +14,50 @@ RES_DIC = {
 }
 
 def response_url(url):
-  status = None
   img_resp = url_request.urlopen(url).read()
-  if len(img_resp) < 100:
-    status = img_resp
-  else:
-    imgnp  =np.array(bytearray(img_resp),dtype=np.uint8)
-    frame = cv2.imdecode(imgnp,-1)
-  return frame, status
+  imgnp  =np.array(bytearray(img_resp),dtype=np.uint8)
+  frame = cv2.imdecode(imgnp,-1)
+  return frame
 
 def print_text(fps, date, time, image):
     
     text = f"{int(fps)} FPS  {date.day}/{date.month}/{date.year}  {time.hour}:{time.minute}:{time.second}"
     font = cv2.FONT_HERSHEY_SIMPLEX
             #   imagen  ,texto ,coordenadas,fuente,tamano  ,BRG        ,grosor
-    cv2.putText(image   ,text  ,(30,30)    ,font  ,1     ,(0,0,0), 2)
+    cv2.putText(image   ,text  ,(30,30)    ,font  ,0.5     ,(0,0,0), 1)
     return image
 
-def main(ESP_IP = "192.168.100.10", res = "mid-high"):
+def main(ESP_IP = "192.168.100.10", res = "high", fps_limit = 7, path_video = 'video_record\\video.mp4'):
 
-  #res_status = url_request.urlopen("http://" + ESP_IP + "/setresolution?res=" + RES_DIC[res])
-  #print(res_status.read())
-  
-  #time.sleep(1)
-  
-  timestamp = 0
+  res_status = url_request.urlopen("http://" + ESP_IP + 
+                                   "/setresolution"  +
+                                   "?width=" + RES_DIC[res]["w"] +
+                                   "&height=" + RES_DIC[res]["h"])
+
+  writer = cv2.VideoWriter(path_video, cv2.VideoWriter_fourcc(*'XIVX'), 10, (int(RES_DIC[res]["w"]), int(RES_DIC[res]["h"])))
+
   fps = 0
   t_prev = tm()
   while True:
     date_and_time = dt.datetime.now()
     date = date_and_time.date()
     time = date_and_time.time()
-    frame, status = response_url("http://" + ESP_IP + "/getpicture")
+    frame = response_url("http://" + ESP_IP + "/getpicture")
 
-    if (tm() - t_prev) < 1:
-      timestamp += 1
-    else:
-      fps = timestamp
-      timestamp = 0
-      t_prev = tm()
-      
-    if (cv2.waitKey(1) == ord("s")) or (status != None):
-      break
     cv2.imshow("frame", print_text(fps, date, time, frame))
+    writer.write(frame)
 
+    if (cv2.waitKey(1) == ord("s")):
+      break
+
+    while(tm() - t_prev) < (1/(fps_limit+1)):
+      pass
+
+    t = tm()
+    fps = 1 / (t - t_prev)
+    t_prev = t
+
+  writer.release()
   cv2.destroyAllWindows()
 
 if __name__ == "__main__":
