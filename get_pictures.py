126b071b67d566124fe19d8c36ec9006a04e2d06
Chuts <jesusesparza242@gmail.com>
2024-02-24 08:49:32 -0600

    record segmented

--- get_pictures.py
+++ get_pictures.py
@@ -8,12 +8,12 @@ from threading import Thread
 
 
 RES_DIC = {
-  "low":       {"w": "320",  "h": "240"},
-  "low-mid":   {"w": "640",  "h": "480"},
-  "mid":       {"w": "800",  "h": "600"},
-  "mid-high":  {"w": "1024", "h": "768"},
-  "high":      {"w": "1280", "h": "1024"},
-  "very-high": {"w": "1600", "h": "1200"}
+  "low":       {"w": 320,  "h": 240},
+  "low-mid":   {"w": 640,  "h": 480},
+  "mid":       {"w": 800,  "h": 600},
+  "mid-high":  {"w": 1024, "h": 768},
+  "high":      {"w": 1280, "h": 1024},
+  "very-high": {"w": 1600, "h": 1200}
 }
 
 def picture_request(url):
@@ -45,43 +45,48 @@ def take_picture(ESP_IP = "192.168.100.3"):
   frame = picture_request("http://" + ESP_IP + "/getpicture")[1]
   cv2.imwrite("picture.jpg", frame)
 
-def main(ESP_IP, res, fps_limit, record, path_video = 'video.mp4'):
+def main(ESP_IP, res, fps_limit, record, video_hours = 1, video_folder = 'videos/'):
   
   w = RES_DIC[res]["w"]
   h = RES_DIC[res]["h"]
 
-  res_status = url_request.urlopen("http://" + ESP_IP + 
-                                   "/setresolution"  +
-                                   "?width=" + w +
-                                   "&height=" + h)
+  res_status = url_request.urlopen("http://"  + ESP_IP + 
+                                   "/setresolution"    +
+                                   "?width="  + str(w) +
+                                   "&height=" + str(h))
+  leave = False
+  while not leave:
+    video_name = f"{dt.datetime.now().ctime()}.mp4"
+    video = cv2.VideoWriter(video_folder + video_name, cv2.VideoWriter_fourcc(*'mp4v'), fps_limit, (w, h))
+    timestamp_limit = 3600 * video_hours * fps_limit
+    timestamp = 0
+    fps = 0
+    t_prev = tm()
+    while timestamp_limit > timestamp:
+      ret, frame = picture_request("http://" + ESP_IP + "/getpicture")
+      if ret:
 
-  video = cv2.VideoWriter(path_video, cv2.VideoWriter_fourcc(*'mp4v'), fps_limit, (int(w), int(h)))
+        date_and_time = dt.datetime.now()
+        cv2.imshow("video", print_text(fps, date_and_time.date(), date_and_time.time(), frame))
 
-  fps = 0
-  t_prev = tm()
-  while True:
-    ret, frame = picture_request("http://" + ESP_IP + "/getpicture")
-    if ret:
+        if record:
+          video.write(frame)
 
-      date_and_time = dt.datetime.now()
-      cv2.imshow("video", print_text(fps, date_and_time.date(), date_and_time.time(), frame))
+        if (cv2.waitKey(1) == ord("s")):
+          leave = True
+          break
 
-      if record:
-        video.write(frame)
+        while(tm() - t_prev) < (1/(fps_limit+1)):
+          pass
 
-      if (cv2.waitKey(1) == ord("s")):
-        break
-
-      while(tm() - t_prev) < (1/(fps_limit+1)):
-        pass
-
-      t = tm()
-      fps = 1 / (t - t_prev)
-      t_prev = t
-
-
-  video.release()
-  cv2.destroyAllWindows()
+        t = tm()
+        fps = 1 / (t - t_prev)
+        t_prev = t
+        
+        timestamp += 1
+        
+    video.release()
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
   argparser = ArgumentParser()
